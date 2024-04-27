1e8542fcb8e5964b3eed9b24476e4b85a2ff301e
BigBigBai <baiyingjian@outlook.com>
2024-04-26 23:57:49 -0400

    234

---
+++ 18/algo-testing/palindrome/palindrome.js
@@ -0,0 +1,14 @@
+// Soln 1
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+//     return str === reversed;
+// }
+
+// Soln 2
+function palindrome(str) {
+    return str.split('').every((char, index) => {
+        return char === str[str.length - index - 1];
+    });
+}
+
+module.exports = palindrome;
